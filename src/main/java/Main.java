beea7566ad427e6f4645a44445ef802bea166393
Adam-Desktop <charltonadam@live.com>
2018-11-12 06:47:39 -0700

    creates a new spreadsheet with the results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -17,12 +20,24 @@ public class Main {
     public static void main(String[] args) throws IOException, InvalidFormatException {
         System.out.println("Hello World");
 
-        Workbook workbook = WorkbookFactory.create(new File(rulesPath));
+        Workbook rulesBook = WorkbookFactory.create(new File(rulesPath));
         testingBook = WorkbookFactory.create(new File(testingPath));
         testingSheet = testingBook.getSheetAt(0);
 
-        Sheet sheet = workbook.getSheetAt(0);
+        Sheet rulesSheet = rulesBook.getSheetAt(0);
         DataFormatter dataFormatter = new DataFormatter();
+
+        Workbook resultBook = new XSSFWorkbook();
+        CreationHelper createHelper = resultBook.getCreationHelper();
+
+        Sheet resultSheet = resultBook.createSheet();
+
+        Font errorFont = resultBook.createFont();
+        errorFont.setBold(true);
+        errorFont.setColor(IndexedColors.RED.getIndex());
+
+        CellStyle errorCellStyle = resultBook.createCellStyle();
+        errorCellStyle.setFont(errorFont);
 
 
 
@@ -31,18 +46,36 @@ public class Main {
 
 
 
-        for(Row row: sheet) {
+        for(Row row: rulesSheet) {
+            Row errorRow = resultSheet.createRow(row.getRowNum());
             Parser.Node n = null;
             for(Cell cell: row) {
                 if(cell.getCellTypeEnum() == CellType.FORMULA) {
                     n = p.parseCell(cell.getCellFormula());
                 }
                 if(n != null) {
+                    double result = testingSheet.getRow(row.getRowNum()).getCell(cell.getColumnIndex()).getNumericCellValue() - n.execute();
                     System.out.println(n.execute() + " == " + testingSheet.getRow(row.getRowNum()).getCell(cell.getColumnIndex()));
+                    Cell errorCell = errorRow.createCell(cell.getColumnIndex());
+                    if(result != 0) {
+                        //Houston, we have a problem, mark it on the results sheet
+
+                        errorCell.setCellValue(result);
+                        errorCell.setCellStyle(errorCellStyle);
+                    }
                 }
             }
-            System.out.println();
         }
+
+        rulesBook.close();
+        testingBook.close();
+
+
+        FileOutputStream fileOut = new FileOutputStream("results.xlsx");
+        resultBook.write(fileOut);
+        fileOut.close();
+
+        resultBook.close();
 
 
 
